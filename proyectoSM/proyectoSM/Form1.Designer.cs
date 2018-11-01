4a20b6f2c06d4865f26382d40221c77bf443aa56
Marco Romero Elizalde <marco.romero@karaokulta.com>
2018-11-01 15:15:07 -0600

    ira 2

--- proyectoSM/proyectoSM/Form1.Designer.cs
+++ proyectoSM/proyectoSM/Form1.Designer.cs
@@ -35,11 +35,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(429, 342);
+            this.button1.Location = new System.Drawing.Point(458, 77);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(171, 58);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "6666666";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
